51aed40d45032be91c78631ace8b0bfa551b76e0
Shivajiraj <shivajiraj53@gmail.com>
2026-05-11 09:19:45 +0530

    Refactor StudentMain servlet implementation

--- StudentMain.java
+++ StudentMain.java
@@ -37,7 +37,9 @@ public class StudentMain extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		
+		//doGet(request, response);
+		
 		response.setContentType("text/html");
 		
 		PrintWriter out = response.getWriter();
